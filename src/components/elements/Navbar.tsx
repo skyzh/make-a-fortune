c5eb0acf82595220355ca105987eb70702015bad
hebingchang <hebingchang1@live.com>
2021-04-08 21:26:17 +0800

    improve: close drawer after navigation

--- src/components/elements/Navbar.tsx
+++ src/components/elements/Navbar.tsx
@@ -10,14 +10,14 @@ import {
   HStack,
   Button,
   Divider,
-  useToast,
+  useToast
 } from "@chakra-ui/react"
 import { Route, NavLink } from "react-router-dom"
 import { useRPCState, useTokenState } from "~/src/settings"
 import { Client } from "~/src/client"
 import {
   ArrowRightShort,
-  ArrowRightCircleFill,
+  ArrowRightCircleFill
 } from "~/src/components/utils/Icons"
 
 const NavButton: React.FC = ({ to, exact, children, ...rest }) => {
@@ -34,6 +34,9 @@ const NavButton: React.FC = ({ to, exact, children, ...rest }) => {
             isFullWidth={true}
             justifyContent="flex-start"
             leftIcon={match ? <ArrowRightCircleFill /> : <ArrowRightShort />}
+            onClick={() => {
+              rest.onClose && rest.onClose()
+            }}
             {...rest}
           >
             {children}
@@ -44,7 +47,7 @@ const NavButton: React.FC = ({ to, exact, children, ...rest }) => {
   )
 }
 
-function Navbar() {
+const Navbar: React.FC = ({ onClose }) => {
   const [rpc, _setRpc] = useRPCState()
   const [token, _setToken] = useTokenState()
   const [backend, setBackend] = useState({})
@@ -56,18 +59,22 @@ function Navbar() {
         setBackend(await client.version())
       }
     }
+
     sendRequest()
-      .then(() => {})
+      .then(() => {
+      })
       .catch((err) =>
         toast({
           title: "无法获取 RPC 后端信息",
           description: `${err}`,
           status: "error",
           duration: 5000,
-          isClosable: true,
+          isClosable: true
         })
       )
   }, [rpc])
+
+  const NB: React.FC = (props) => <NavButton {...props} onClose={onClose} />
 
   return (
     <Stack spacing={1}>
@@ -76,26 +83,26 @@ function Navbar() {
         <Heading fontSize="lg">闷声发财</Heading>
       </HStack>
 
-      <NavButton exact to="/">
+      <NB exact to="/">
         最新
-      </NavButton>
-      <NavButton to="/posts/trend">趋势</NavButton>
+      </NB>
+      <NB to="/posts/trend">趋势</NB>
       <Divider />
       <Box px="5">
         <Text color="gray.500" fontSize="sm">
           个人
         </Text>
       </Box>
-      <NavButton to="/posts/star">收藏</NavButton>
-      <NavButton to="/posts/notification">通知</NavButton>
-      <NavButton to="/posts/me">我的发帖</NavButton>
+      <NB to="/posts/star">收藏</NB>
+      <NB to="/posts/notification">通知</NB>
+      <NB to="/posts/me">我的发帖</NB>
       <Divider />
       <Box px="5">
         <Text color="gray.500" fontSize="sm">
           系统
         </Text>
       </Box>
-      <NavButton to="/login">{token ? "切换用户" : "登录"}</NavButton>
+      <NB to="/login">{token ? "切换用户" : "登录"}</NB>
       <Box pt="5">
         <Text color="gray.500" fontSize="xs">
           「闷声发财」是一个通用的匿名社区前端。
--- src/components/elements/PhoneNavbar.tsx
+++ src/components/elements/PhoneNavbar.tsx
@@ -53,7 +53,7 @@ const PhoneNavbar: React.FC = () => {
             <DrawerCloseButton />
             <DrawerHeader />
             <DrawerBody>
-              <Navbar />
+              <Navbar onClose={onClose} />
             </DrawerBody>
           </DrawerContent>
         </DrawerOverlay>

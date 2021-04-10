42c86b804c41cd56ea9d913fef4f5627777725c3
BugenZhao <i@bugenzhao.com>
2021-04-10 12:56:23 +0800

    add bgcolor for logo

--- src/components/widgets/Logo.tsx
+++ src/components/widgets/Logo.tsx
@@ -1,9 +1,11 @@
-import { Square, Text } from "@chakra-ui/react"
+import { Square, Text, useColorModeValue } from "@chakra-ui/react"
 import React from "react"
 
 function Logo() {
+  const bgColor = useColorModeValue("white", "gray.700")
+
   return (
-    <Square size="35px" shadow="lg" borderRadius="md">
+    <Square size="35px" shadow="lg" borderRadius="md" bgColor={bgColor}>
       <Text p="1">
         <span className="maf-logo">發</span>
       </Text>

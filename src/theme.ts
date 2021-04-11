82d18a28ff811d0a8985266a3f72a04e0a757b38
Alex Chi <iskyzh@gmail.com>
2021-04-11 14:40:22 +0800

    use fonts.css

--- src/theme.ts
+++ src/theme.ts
@@ -5,4 +5,11 @@ export const theme = extendTheme({
     initialColorMode: "light",
     useSystemColorMode: false,
   },
+  // From fonts.scss
+  fonts: {
+    heading:
+      '-apple-system, "Noto Sans", "Helvetica Neue", Helvetica, "Nimbus Sans L", Arial, "Liberation Sans", "PingFang SC", "Hiragino Sans GB", "Noto Sans CJK SC", "Source Han Sans SC", "Source Han Sans CN", "Microsoft YaHei", "Wenquanyi Micro Hei", "WenQuanYi Zen Hei", "ST Heiti", SimHei, "WenQuanYi Zen Hei Sharp", sans-serif',
+    body:
+      '-apple-system, "Noto Sans", "Helvetica Neue", Helvetica, "Nimbus Sans L", Arial, "Liberation Sans", "PingFang SC", "Hiragino Sans GB", "Noto Sans CJK SC", "Source Han Sans SC", "Source Han Sans CN", "Microsoft YaHei", "Wenquanyi Micro Hei", "WenQuanYi Zen Hei", "ST Heiti", SimHei, "WenQuanYi Zen Hei Sharp", sans-serif',
+  },
 })

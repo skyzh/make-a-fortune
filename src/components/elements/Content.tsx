94f0d178501b5adfc4bb89efd1d0a3e3b022ebf0
hebingchang <hebingchang1@live.com>
2021-04-11 14:51:36 +0800

    feat: enable link in post content

--- src/components/elements/Content.tsx
+++ src/components/elements/Content.tsx
@@ -1,12 +1,38 @@
-import { Box, Collapse, Flex, Text } from "@chakra-ui/react"
+import { Box, Collapse, Flex, Link, Text } from "@chakra-ui/react"
 import React from "react"
+// @ts-ignore as the library does not have a type definition
+import processString from "react-process-string"
+import { NavLink } from "react-router-dom"
+
+let renderer = processString([
+  {
+    regex: /wkfg:\/\/\d{6}/gim,
+    fn: (key: number, result: string[]) => {
+      const postId = result[0].substring(7)
+      return (
+        <span key={key}>
+          <Link color="teal.500" as={NavLink} to={`/posts/${postId}`}>
+            #{postId}
+          </Link>
+        </span>
+      )
+    }
+  },
+  {
+    regex: /(http|https):\/\/(\S+)\.([a-z]{2,}?)(.*?)( |\,|$|\.)/gim,
+    fn: (key: number, result: string[]) => <span key={key}>
+                                     <Link color="teal.500" isExternal
+                                           href={`${result[1]}://${result[2]}.${result[3]}${result[4]}`}>{result[2]}.{result[3]}{result[4]}</Link>{result[5]}
+                                 </span>
+  }
+])
 
 export function Content({ content }: { content: string }) {
   return (
     <Box>
       {content.split("\n").map((line, index) => (
         <Text mt={0} wordBreak="break-word" key={index}>
-          {line}
+          {renderer(line)}
         </Text>
       ))}
     </Box>
@@ -14,11 +40,11 @@ export function Content({ content }: { content: string }) {
 }
 
 export function CollapseContent({
-  content,
-  collapsed,
-  minLines = "auto",
-  maxLines = "auto",
-}: {
+                                  content,
+                                  collapsed,
+                                  minLines = "auto",
+                                  maxLines = "auto"
+                                }: {
   content: string
   collapsed: boolean
   minLines?: number | "auto"

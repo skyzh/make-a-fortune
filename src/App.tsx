96207e3027615afe75210d32d542aec8946889d5
Alex Chi <iskyzh@gmail.com>
2021-08-27 17:57:25 +0800

    *: add basic layout (#83)
    Signed-off-by: Alex Chi <iskyzh@gmail.com>

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,75 @@
-import React from 'react';
-import { NativeBaseProvider, Text, Box } from 'native-base';
+import React, { useState } from 'react';
+import {
+  NativeBaseProvider,
+  Text,
+  VStack,
+  Badge,
+  HStack,
+  FlatList,
+} from 'native-base';
 import { registerRootComponent } from 'expo';
+import range from 'lodash/range';
+import map from 'lodash/map';
+import { RefreshControl } from 'react-native-web-refresh-control';
+
+const data = map(range(100), x => ({
+  id: x,
+  title: '我的失败，彻彻底底',
+  content: '面对命中注定的失败',
+}));
+
+function wait(timeout) {
+  return new Promise(resolve => {
+    setTimeout(resolve, timeout);
+  });
+}
 
 function App(): JSX.Element {
+  const [refreshing, setRefreshing] = useState(false);
+
+  const reloadLines = React.useCallback(() => {
+    setRefreshing(true);
+
+    wait(2000).then(() => {
+      setRefreshing(false);
+    });
+  }, []);
+
   return (
     <NativeBaseProvider>
-      <Box flex={1} bg="#fff" alignItems="center" justifyContent="center">
-        <Text>Hello, World!</Text>
-      </Box>
+      <FlatList
+        safeArea
+        userSelect="none"
+        refreshControl={
+          <RefreshControl refreshing={refreshing} onRefresh={reloadLines} />
+        }
+        initialNumToRender={5}
+        data={data}
+        renderItem={({ item }) => (
+          <VStack
+            borderColor="gray.300"
+            borderWidth={1}
+            py={4}
+            px={3}
+            my={2}
+            rounded="md"
+            alignSelf="center"
+            width={700}
+            maxWidth="100%"
+            key={item.id}
+            space={1}
+          >
+            <Text fontSize="sm">
+              <Badge>#23333</Badge>
+            </Text>
+            <HStack space={1} alignItems="center">
+              <Text>{item.title}</Text>
+            </HStack>
+            <Text>{item.content}</Text>
+          </VStack>
+        )}
+        keyExtractor={item => item.id}
+      />
     </NativeBaseProvider>
   );
 }
